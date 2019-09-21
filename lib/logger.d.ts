f6b79287616645299d4f4291f0cffc3c795f31de
Steve Jabour <steve@jabour.me>
2019-09-20 18:54:32 -0600

    Added createLogger method

--- lib/logger.d.ts
+++ lib/logger.d.ts
@@ -1,2 +1,4 @@
+import { FilePath } from './package';
 import * as Bunyan from 'bunyan';
 export declare const logger: Bunyan;
+export declare function createLogger(cwd: FilePath, level?: Bunyan.LogLevel): Bunyan;
--- lib/logger.ts
+++ lib/logger.ts
@@ -1,6 +1,12 @@
-import { readPackageSync } from './package';
+import { readPackageSync, FilePath } from './package';
 import * as Bunyan from 'bunyan';
 
 const { name } = readPackageSync();
 
 export const logger: Bunyan = Bunyan.createLogger({ level: 'info', name });
+
+export function createLogger(cwd: FilePath, level?: Bunyan.LogLevel): Bunyan {
+  const { name } = readPackageSync(cwd);
+
+  return Bunyan.createLogger({ level: (level || 'info'), name });
+}
